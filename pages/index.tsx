c5f6e6c26920b142b41eeac55462446cb93c3d08
washingtonguilhardes <washington.guilhardes@gmail.com>
2021-03-31 17:03:33 -0300

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="main">
+      <strong>Lojinha de filmes: </strong> <span>em breve</span>
+    </div>
+  );
+};
+
+export default Home;
